c196bbd9942f0d4c559f890fe9896473652af148
zaidrauf7 <iamzaidrauf@gmail.com>
2024-05-19 13:29:25 +0500

    Form change flex add

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,21 +2,21 @@ const Form = ({ setTitle }) => {
   return (
     <div className="border-2 border-gray-400 w-full h-screen flex justify-center items-center ">
       <div className="flex-col justify-center">
-      <div>
+      <div className="flex items-center justify-between mb-8">
         <h1>
             Title
         </h1>
         <input className="border-2 p-2 rounded-md w-[250px] border-gray-700" type="text" onChange={e => setTitle(e.target.value)}/>
 
       </div>
-      <div>
+      <div className="flex items-center justify-between mb-8">
         <h1>
             Description
         </h1>
         <input className="border-2 p-2 rounded-md w-[250px] border-gray-700" type="text" onChange={e => setTitle(e.target.value)}/>
 
       </div>
-      <div>
+      <div className="flex items-center justify-between mb-8">
         <h1>
             Image
         </h1>
